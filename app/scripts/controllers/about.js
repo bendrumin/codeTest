d2c8c0af126804eb0b2cf5041d3cfed825de551f
Brent <blthiewes@gmail.com>
2019-03-11 18:01:09 -0500

    add initial framework

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codeChallengeTemplateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the codeChallengeTemplateApp
+ */
+angular.module('codeChallengeTemplateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codeChallengeTemplateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the codeChallengeTemplateApp
+ */
+angular.module('codeChallengeTemplateApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
