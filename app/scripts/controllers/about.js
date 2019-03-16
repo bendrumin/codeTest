c6da38528f82983f20d1ee114802728403938ef6
Ben Siegel <bensiegel@Bens-MacBook-Pro.local>
2019-03-16 17:17:38 -0500

    Initial commit, code doesnt work but I am testing it out and getting errors

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,11 +7,20 @@
  * # AboutCtrl
  * Controller of the codeChallengeTemplateApp
  */
-angular.module('codeChallengeTemplateApp')
+angular.module('codeChallengeTemplateApp', ["ngTable"])
   .controller('AboutCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    function TableCtrl($scope) {
+    $scope.testVar = 'This my test for ngtable';
+    // if this is your controller, every var you put in the $scope will be accessible in the view
+
+    var data = [{name: "Moroni", age: 50} /*,*/];
+    $scope.tableParams = new NgTableParams({}, { dataset: data});
+}
+TableCtrl.$inject = ["$scope","NgTableParams", "ngTableSimpleList"];
+module.exports = TableCtrl;
   });
