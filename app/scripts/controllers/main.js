5532c3ccb92ef259ed43291546ed36822bcc03a8
Ben Siegel <bensiegel@Bens-MacBook-Pro.local>
2019-03-17 16:58:38 -0500

    Fixing existing code based off of help from someone at Prime

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,28 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name codeChallengeTemplateApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the codeChallengeTemplateApp
- */
+* @ngdoc function
+* @name codeChallengeTemplateApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the codeChallengeTemplateApp
+*/
 angular.module('codeChallengeTemplateApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope) {
+  this.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+  $scope.sortType     = 'name'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
 
-  var app = angular.module('myApp', []);
-  app.controller('MainCtrl', function($scope, $http) {
-      $http.get('../../test-data/designs.csv')
-      .then(function (response) {$scope.names = response.data.records;});
-  });
+  // create the list of sushi rolls
+  $scope.cambria = [
+    { name: 'Baybridge', created: ' 2019-02-01', status:  'R&D' },
+    { name: 'Bently', created: ' 2018-11-01', status:  'R&D' },
+    { name: 'Islington', created: ' 2018-10-30', status: 'Deprecated' },
+    { name: 'Marwell', created: ' 2017-06-04', status: ' Released' }
+  ];
+
+});
